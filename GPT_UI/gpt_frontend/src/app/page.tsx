ee352456a692247f3c18f34efbfa837ad43fe07e
mdkhaledbin <mdkhaledbin1221@gmail.com>
2025-02-10 09:01:07 +0600

    sidebar navbar added

---
+++ GPT_UI/gpt_frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen w-full p-16 sm:p-20 bg-gray-900 dark:bg-gray-900">
+      <h1>Hi there! 👋</h1>
+    </div>
+  );
+}
